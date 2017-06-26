9c3d345f104861c912731427f1802a7c437b872d
Tiago Engel <tengel-bd@instructure.com>
2017-06-25 22:14:16 -0300

    prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const fullPath = pathName => path.join(__dirname, pathName)
+const fullPath = pathName => path.join(__dirname, pathName);
+
+const env = process.env.NODE_ENV || 'dev';
+const prod = env === 'prod';
+
+const plugins = [
+  new webpack.NamedModulesPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(env)
+    }
+  })
+];
+
+if (prod) {
+  plugins.push(new ExtractTextPlugin('styles.css'));
+}
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: prod ? false : 'cheap-module-eval-source-map',
   entry: fullPath('src/index.js'),
   output: {
     path: fullPath('dist'),
@@ -12,34 +30,36 @@ module.exports = {
   },
   devServer: {
     contentBase: fullPath('src'),
-    publicPath: 'http://localhost:8080/assets/'
+    publicPath: 'http://localhost:8080/'
   },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('development')
-      }
-    })
-  ],
+  plugins,
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: [/node_modules/, /css/],
-        include: path.join(__dirname, 'public'),
+        include: path.join(__dirname, 'src'),
         use: ['babel-loader']
       },
       {
         test: /\.scss$/,
         include: path.join(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+        use: prod
+          ? ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader', 'sass-loader']
+          })
+          : ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
       },
       {
         test: /\.css$/,
         include: [/node_modules/],
-        use: ['style-loader', 'css-loader', 'postcss-loader']
+        use: prod
+          ? ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader']
+          })
+          : ['style-loader', 'css-loader', 'postcss-loader']
       },
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
